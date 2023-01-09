e48cfda33096f0ac8e48165589d956555222ac3f
Anh Tuan <atn832@gmail.com>
2023-01-09 10:39:59 +0900

    move comment to fake_firestore

--- lib/src/fake_firebase_security_rules.dart
+++ lib/src/fake_firebase_security_rules.dart
@@ -13,16 +13,6 @@ class FakeFirebaseSecurityRules {
 
   bool isAllowed(String path, Method method,
       {Map<String, dynamic> variables = const {}}) {
-    // TODO: populate `request` and `resource`.
-    // https://firebase.google.com/docs/rules/rules-language#building_conditions
-    // https://firebase.google.com/docs/reference/rules/rules.firestore.Request
-    // https://firebase.google.com/docs/reference/rules/rules.firestore.Resource
-    // `resource` works with get `get` and `exists` custom functions. Enables
-    // this kinds of expressions:
-    // `get(/databases/(database)/documents/users/$(request.auth.uid)).data.admin)`
-    // https://firebase.google.com/docs/rules/rules-language#function
-    // TODO: populate `request` with `auth`.
-    // https://firebase.google.com/docs/rules/rules-and-auth
     for (final match in service.pathMatches) {
       if (match.isAllowed(path.concretePathSegments, method,
           variables: variables)) {
